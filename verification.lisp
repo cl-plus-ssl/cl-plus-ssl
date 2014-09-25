81c94a72d9e98218294301d69560e7db405f53a4
Ilya Khaprov <dead.trickster@gmail.com>
2014-09-24 18:57:06 -0700

    fixes

--- verification.lisp
+++ verification.lisp
@@ -47,86 +47,27 @@
 ;;       (if x509-vp
 ;;           (X509-VERIFY-PARAM-free x509-vp)))))
 
-(cffi:defcallback cb-ssl-verify :int ((ok :int) (ctx :pointer))
-  (let* (;(certificate (X509-STORE-CTX-get-current-cert ctx))
-        (error-code (X509_STORE_CTX-get-error ctx)))
-    (unless (eql error-code 0)
-      (error 'ssl-error-verify  :error-code error-code))
-    ok))
+(defun set-flags (&rest flags)
+  (apply #'logior flags))
 
-(cffi:defcfun ("sk_value" sk-value)
-    :pointer
-  (stack :pointer)
-  (index :int))
+(defun add-flag (flags flag)
+  (logior flags flag))
 
-(cffi:defcfun ("sk_num" sk-num)
-    :int
-  (stack :pointer))
+(defun remove-flag (flags flag)
+  (logand flags (lognot flag)))
 
-(cffi:defcfun ("X509_NAME_get_index_by_NID" x509-name-get-index-by-nid)
-    :int
-  (name :pointer)
-  (nid :int)
-  (lastpost :int))
-
-(cffi:defcfun ("X509_NAME_get_entry" x509-name-get-entry)
-    :pointer
-  (name :pointer)
-  (log :int))
-
-(cffi:defcfun ("X509_NAME_ENTRY_get_data" x509-name-entry-get-data)
-    :pointer
-  (name-entry :pointer))
-
-(cffi:defcfun ("ASN1_STRING_data" asn1-string-data)
-    :pointer
-  (asn1-string :pointer))
-
-(cffi:defcfun ("ASN1_STRING_length" asn1-string-length)
-    :int
-  (asn1-string :pointer))
-
-(cffi:defcfun ("ASN1_STRING_type" asn1-string-type)
-    :int
-  (asn1-string :pointer))
-
-(cffi:defcfun ("strlen" strlen)
-    :int
-  (string :string))
-
-
-;; GENERAL-NAME types
-(defconstant +GEN-OTHERNAME+  0)
-(defconstant +GEN-EMAIL+  1)
-(defconstant +GEN-DNS+    2)
-(defconstant +GEN-X400+ 3)
-(defconstant +GEN-DIRNAME+  4)
-(defconstant +GEN-EDIPARTY+ 5)
-(defconstant +GEN-URI+    6)
-(defconstant +GEN-IPADD+  7)
-(defconstant +GEN-RID+    8)
-
-(defun sk-general-name-value (names index)
-  (sk-value names index))
-
-(defun sk-general-name-num (names)
-  (sk-num names))
-
-(cffi:defcstruct asn1_string_st
-  (length :int)
-  (type :int)
-  (data :pointer)
-  (flags :long))
-
-(cffi:defcstruct GENERAL_NAME
-  (type :int)
-  (data :pointer))
+(defun flag-set-p (flags flag &optional match)
+  (let ((r (logand flags flag)))
+    (if (> r 0)
+        (if match
+            (= r flag)
+            t)
+        nil)))
 
 (defun remove-trailing-dot (str)
   (if (eql (elt str (1- (length str))) #\.)
       (subseq str 0 (- (length str) 2))
       str))
-
 
 #|
 http://tools.ietf.org/html/rfc6125
@@ -151,181 +92,191 @@ character is embedded within an A-label or U-label [IDNA-DEFS] of
 an internationalized domain name [IDNA-PROTO].
 |#
 
-(defun try-match-using-wildcards (hostname pattern)
+(defun try-match-using-wildcards (pattern hostname)
+  ;; wildcard must match at least one character
+  (when (> (length pattern) (length hostname))
+    (return-from try-match-using-wildcards nil))
+
   (let ((pattern-w-pos (position #\* pattern))
-        (pattern-leftmost-label-end)
-        (hostname-leftmost-label-end))
+        (pattern-leftmost-label-end))
     (unless pattern-w-pos
       (return-from try-match-using-wildcards nil))
 
-    ;; TODO: detect if hostname is IP address
+    ;; only one star
+    (when (position #* pattern :start pattern-w-pos)
+      (return-from try-match-using-wildcards nil))
 
+    ;; check if pattern has at least two dots after star
+    ;; check no labels(dots) behind star
     (setq pattern-leftmost-label-end (position #\. pattern))
-    (when (or (null pattern-leftmost-label-end) (null (position #\. pattern :start (1+ pattern-leftmost-label-end)))
-              (> pattern-w-pos pattern-leftmost-label-end)
-              (string= pattern "xn--" :end1 4))
+    (when (or (null pattern-leftmost-label-end)
+              (null (position #\. pattern :start (1+ pattern-leftmost-label-end)))
+              (> pattern-w-pos pattern-leftmost-label-end))
       (return-from try-match-using-wildcards nil))
 
-    (setf hostname-leftmost-label-end (position #\. hostname))
-    (when (or (null hostname-leftmost-label-end) (not (string= hostname pattern :start1 hostname-leftmost-label-end
-                                                                                :start2 pattern-leftmost-label-end)))
-
+    ;;check star is not part of A-label label, what about U-labels though?
+    (when (search "xn--" pattern :test #'equal :end2 pattern-leftmost-label-end)
       (return-from try-match-using-wildcards nil))
 
+    (let* ((pattern-length-after-star (- (length pattern) pattern-w-pos 1 #|not include star|#))
+           (hostname-position-after-star (- (length hostname) pattern-length-after-star)))
+      ;; do not allow *.example.com match bar.foo.example.com
+      ;; also here we can partially match part after star
+      (if (and (not (position #\. hostname :end hostname-position-after-star))
+               (string-equal hostname pattern :start1  hostname-position-after-star :start2 (1+ pattern-w-pos)))
+          (if (= 0 pattern-w-pos)
+              t
+              ;; match before star part now
+              (string-equal hostname pattern :end1 pattern-w-pos :end2 pattern-w-pos))
+          nil))))
 
-    (when (< hostname-leftmost-label-end pattern-leftmost-label-end)
-      (return-from try-match-using-wildcards nil))
+(defun skip-prefix (pattern subject flags)
+  (unless (flag-set-p flags +_x509-check-flag-dot-subdomains+)
+    (return-from skip-prefix 0))
+  #|
+  * If subject starts with a leading '.' followed by more octets, and
+  * pattern is longer, compare just an equal-length suffix with the
+  * full subject (starting at the '.')
+  *|#
 
-    t))
+  (let ((pattern-length (length pattern)))
+    (unless (> pattern-length (length subject))
+      (return-from skip-prefix 0))
+    (do ((i 0 (1+ i)))
+        ((or (<= pattern-length (length subject)) (= pattern-length 0)) i)
+      (when (and (eql (aref pattern i) #\.)
+                 (flag-set-p flags +x509-check-flag-single-label-subdomains+))
+        (return i))
+      (decf pattern-length))))
 
-(defun verify-hostname% (hostname pattern)
-  (log:debug "Verifying ~A against ~A" hostname pattern)
-  (setf hostname (remove-trailing-dot hostname)
-        pattern (remove-trailing-dot pattern))
-  (if (string= hostname pattern)
+(defun just-equal (pattern subject flags)
+  (declare (ignore flags))
+  (equal pattern subject))
+
+(defun equal-case (pattern subject flags)
+  (let ((pattern-start (skip-prefix pattern subject flags)))
+    (string= pattern subject :start1 pattern-start)))
+
+(defun equal-nocase (pattern subject flags)
+  (let ((pattern-start (skip-prefix pattern subject flags)))
+    (string-equal pattern subject :start1 pattern-start)))
+
+(defun equal-wildcard (pattern subject flags)
+  (if (equal-nocase subject pattern flags)
       t
-      (try-match-using-wildcards hostname pattern)))
+      (try-match-using-wildcards pattern subject)))
 
-(defun try-get-asn1-string-data (asn1-string) ;; as for now valid for GEN_DNS GEN_EMAIL etc..
-  (cffi:with-foreign-slots ((length data) asn1-string (:struct asn1_string_st))
-    (let* ((strlen (strlen data)))
-      (when (= strlen length)
-        (cffi:foreign-string-to-lisp data)))))
+(defun equal-email (pattern subject flags)
+  (declare (ignore flags))
+  (let* ((length (length pattern))
+         (i length))
+    (when (/= length (length subject))
+      (return-from equal-email nil))
 
-(defun try-match-alt-name (certificate hostname)
-  (let ((altnames (x509-get-ext-d2i certificate 85 #|NID_subject_alt_name|# (cffi:null-pointer) (cffi:null-pointer)))
-        (matched nil)
-        (alt-names-collection (list)))
-    (if (not (cffi:null-pointer-p altnames))
-        (prog1
-            (let ((altnames-count (sk-general-name-num altnames)))
-              (do ((i 0 (1+ i)))
-                  ((or (eq t matched) (>= i  altnames-count)) matched)
-                (let* ((name (sk-general-name-value altnames i))
-                       (dns-name))
-                  (cffi:with-foreign-slots ((type data) name (:struct general_name))
-                    (when (= type 2 #|GEN_DNS|#)
-                      (setq dns-name (try-get-asn1-string-data data))
-                      (when dns-name
-                        (setf alt-names-collection (append alt-names-collection (list dns-name)))
-                        (setq matched (if (verify-hostname% hostname dns-name) t :no-alt-match))))))))
-          ;; turns out sk_GENERAL_NAME_pop_free is layered #define mess, don't know what to do now
-          ;;(sk_GENERAL_NAME_pop_free altnames 1216 #|GENERAL_NAME_free|#)
-          ))
-    (if (eq :no-alt-match matched)
-        (error 'ssl-unable-to-match-alternative-name :hostname hostname
-                                                     :found-alt-names alt-names-collection)
-        matched)))
+    #|We search backwards for the '@' character, so that we do
+    not have to deal with quoted local-parts.  The domain part
+    is compared in a case-insensitive manner.|#
+    (do ((ret))
+        ((or ret (<= i 0)))
+      (decf i)
+      (when (or (eql (aref pattern i) #\@) (eql (aref subject i) #\@))
+        (setf ret t)
+        (unless (string-equal pattern subject :start1 i :start2 i)
+          (return-from equal-email nil))))
+    (string= pattern subject :end1 i :end2 i)))
 
-(defun get-common-name-index (certificate)
-  (x509-name-get-index-by-nid (x509-get-subject-name certificate) 13 #|NID_commonName|# -1))
 
-(defun get-common-name-entry (certificate index)
-  (x509-name-get-entry (x509-get-subject-name certificate) index))
 
-(defun try-match-common-name (certificate hostname)
-  (log:info "try-match-common-name")
-  (let (common-name-index
-        common-name-entry
-        common-name-asn1
-        dns-name)
-    (setf common-name-index (get-common-name-index certificate))
-    (unless common-name-index
-      (error 'ssl-unable-to-obtain-common-name))
-    (setf common-name-entry (get-common-name-entry certificate common-name-index))
-    (unless common-name-entry
-      (error 'ssl-unable-to-obtain-common-name))
-    (setf common-name-asn1 (x509-name-entry-get-data common-name-entry))
-    (unless common-name-asn1
-      (error 'ssl-unable-to-obtain-common-name))
-    (setq dns-name (try-get-asn1-string-data common-name-asn1))
-    (unless dns-name
-      (error 'ssl-unable-to-obtain-common-name))
-    (unless (verify-hostname% hostname dns-name)
-      (error 'ssl-unable-to-match-common-name :hostname hostname :found-common-name dns-name))))
+(defun try-match-pattern (pattern subject equality-function flags)
+  (let ((pattern-string (try-get-asn1-string-data pattern)))
+    (unless pattern-string
+      (return-from try-match-pattern (values nil :invalid-pattern-string)))
+    (if (funcall equality-function pattern-string subject flags)
+        (values t pattern-string))))
 
-(defun get-alt-names (certificate alt-name-type)
-  (let ((altnames (x509-get-ext-d2i certificate 85 #|NID_subject_alt_name|# (cffi:null-pointer) (cffi:null-pointer))))
-    (alt-names-collection (list)))
-  (if (not (cffi:null-pointer-p altnames))
-      (prog1
-          (let ((altnames-count (sk-general-name-num altnames)))
-            (do ((i 0 (1+ i)))
-                ((or (eq t matched) (>= i  altnames-count)) alt-names-collection)
-              (let* ((alt-name (sk-general-name-value altnames i))
-                     (alt-name-string))
-                (cffi:with-foreign-slots ((type data) alt-name (:struct general_name))
-                  (when (= type alt-name-type)
-                    (setq alt-name-string (try-get-asn1-string-data data)) 
-                    (when alt-name-string
-                      (setf alt-names-collection (append alt-names-collection (listcheck-string)))))))))
-        ;; turns out sk_GENERAL_NAME_pop_free is layered #define mess, don't know what to do now
-        ;;(sk_GENERAL_NAME_pop_free altnames 1216 #|GENERAL_NAME_free|#)
-        )))
+(defun try-match (pattern subject asn1-string-type equality-function flags)
+  (when (or (cffi:null-pointer-p (asn1-string-data pattern))
+            (= 0 (asn1-string-length pattern)))
+    (return-from try-match (values nil :invalid-pattern-structure)))
 
-(()
+  (if (> asn1-string-type 0)
+      (cond
+        ((/= (asn1-string-type pattern) asn1-string-type)
+         (return-from try-match (values nil :wrong-pattern-type)))
+        ((= (asn1-string-type pattern) +V-ASN1-IASTRING+)
+         (try-match-pattern pattern subject equality-function flags))
+        (t (try-match-pattern pattern subject #'just-equal flags)))
+      (try-match-pattern pattern subject equality-function flags)))
 
-(defun do-x509-check (certificate chk type &key (always-check-subject t)
-                                                (no-wildcards nil)
-                                                (no-partial-wildcards t)
-                                                (multi-label-wildcards nil)
-                                                (single-label-subdomain t))
+(defconstant +NID-commonName+   13)
+(defconstant +NID-pkcs9-emailAddress+   48)
 
-  (let ((altnames (get-alt-names certificate type))
-        common-name-id
-        asn1-string-type
-        equality-function)
-    (case type
-      (+GEN-EMAIL+
-       (setf common-name-id +NID-pks9-emailAddress
+(defun do-x509-check (certificate chk chk-type flags)
+  (setq flags (remove-flag flags +_X509-CHECK-FLAG-DOT-SUBDOMAINS+))
+  (let (common-name-id asn1-string-type equality-function)
+    (cond
+      ((eql chk-type +GEN-EMAIL+)
+       (setf common-name-id +NID-pkcs9-emailAddress+
              asn1-string-type +V-ASN1-IASTRING+
              equality-function 'equal-email))
-      (+GEN-DNS+
+      ((eql chk-type +GEN-DNS+)
        (setf common-name-id +NID-commonName+
              asn1-string-type +V-ASN1-IASTRING+
-             equality-function (if no-wildcards
+             equality-function (if (flag-set-p flags +x509-check-flag-no-wildcards+)
                                    'equal-nocase
-                                   'equal-wildcard)))
-      (t (setf alt-type +V-ASN1-OCTET-STRING+
-               equality-function equal-case)))
-    
-    (if (= 0 (length altnames)
-           (if (or (not common-name-id) (not always-check-subject))
-               (values :no-alt-match altnames)
-               (let ((common-name (get-common-name certificate)))
-                 (if common-name
-                     (try-match common-name chk type asn1-string-type equality-function
-                                :always-check-subject always-check-subject
-                                :no-wildcards no-wildcards
-                                :no-partial-wildcards no-partial-wildcards
-                                :multi-label-wildcards multi-label-wildcards
-                                :single-label-subdomain single-label-subdomain)
-                     (values :no-common-name))))
-           (let ((check-result))
-             (dolist (altname altnames check-result)
-               (try-match alt-name chk type asn1-string-type equality-function
-                          :always-check-subject always-check-subject
-                          :no-wildcards no-wildcards
-                          :no-partial-wildcards no-partial-wildcards
-                          :multi-label-wildcards multi-label-wildcards
-                          :single-label-subdomain single-label-subdomain)))))))
+                                   'equal-wildcard))
+       (if (and (> (length chk)1)
+                (eql #\. (aref chk 0)))
+           (setf flags (add-flag flags +_X509-CHECK-FLAG-DOT-SUBDOMAINS+))))
+      (t (setf asn1-string-type +V-ASN1-OCTET-STRING+
+               equality-function 'equal-case
+               common-name-id 0)))
+
+    ;; first try match altname if any
+    (let ((altnames (x509-get-ext-d2i certificate 85 #|NID_subject_alt_name|# (cffi:null-pointer) (cffi:null-pointer))))
+      (if (not (cffi:null-pointer-p altnames))
+          (unwind-protect
+               (progn (let ((altnames-count (sk-general-name-num altnames)))
+                        (do ((i 0 (1+ i)))
+                            ((>= i altnames-count))
+                          (let* ((alt-name (sk-general-name-value altnames i)))
+                            (cffi:with-foreign-slots ((type data) alt-name (:struct general_name))
+                              (when (= type chk-type)
+                                (multiple-value-bind (result reason)
+                                    (try-match data chk asn1-string-type equality-function flags)
+                                  (if result
+                                      (return-from do-x509-check (values t reason)))))))))
+                      (when (and (not common-name-id)
+                                 (not (flag-set-p flags +X509-check-flag-always-check-subject+)))
+                        (return-from do-x509-check (values nil :no-alt-name-match))))
+            ;; turns out sk_GENERAL_NAME_pop_free is layered #define mess, don't know what to do now
+            ;;(sk_GENERAL_NAME_pop_free altnames 1216 #|GENERAL_NAME_free|#)
+            )))
+    ;; no alt names or always check subject
+    (let ((i -1)
+          (subject-name (x509-get-subject-name certificate)))
+      (loop
+        (setf i (x509-name-get-index-by-nid subject-name common-name-id i))
+        (when (< i 0)
+          (return (values nil :no-common-name-match))
+          (let* ((entry (x509-name-get-entry subject-name i)))
+            (multiple-value-bind (result reason)
+                (try-match (x509-name-entry-get-data entry) chk -1 equality-function)
+              (if result
+                  (return-from do-x509-check (values result reason))))))))))
 
 
-(defun verify-hostname (certificate hostname &key (always-check-subject t)
-                                                  (no-wildcards nil)
-                                                  (no-partial-wildcards t)
-                                                  (multi-label-wildcards nil)
-                                                  (single-label-subdomain t))
-  (do-x509-check certificate hostname +GEN-DNS+
-    :always-check-subject always-check-subject
-    :no-wildcards no-wildcards
-    :no-partial-wildcards no-partial-wildcards
-    :multi-label-wildcards multi-label-wildcards
-    :single-label-subdomain single-label-subdomain))
+(defun verify-hostname (certificate hostname &optional (flags (set-flags +X509-CHECK-FLAG-ALWAYS-CHECK-SUBJECT+
+                                                                         +X509-CHECK-FLAG-NO-PARTIAL-WILDCARDS+
+                                                                         +X509-CHECK-FLAG-SINGLE-LABEL-SUBDOMAINS+)))
+  (do-x509-check certificate hostname +GEN-DNS+ flags))
 
 
-(defun verify-emial (certificate email &key (always-check-subject t)))
+(defun verify-emial (certificate email &optional (flags +X509-CHECK-FLAG-ALWAYS-CHECK-SUBJECT+))
+  (do-x509-check certificate email +GEN-EMAIL+ flags))
 
-(defun verify-ip (certificate ip &key (always-check-subject t)))
+(defun verify-ip (certificate ip &optional (flags +X509-CHECK-FLAG-ALWAYS-CHECK-SUBJECT+))
+  (do-x509-check certificate ip +GEN-IPADD+ flags))
 
-(defun verify-ip-asc (certificate ip &key (always-check-subject t)))
+(defun verify-ip-asc (certificate ip &optional (flags +X509-CHECK-FLAG-ALWAYS-CHECK-SUBJECT+))
+  (error "Not implemented"))
