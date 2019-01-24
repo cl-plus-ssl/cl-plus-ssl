136757120966d8f7ba5c4600af6111863802c6ef
Anton Vodonosov <avodonosov@yandex.ru>
2019-01-24 11:15:54 +0300

    run-on-many-lisps-and-openssls/openssl-releases/ : fetch-all.sh build-all.sh

---
+++ test/run-on-many-lisps-and-openssls/openssl-releases/build-all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "`dirname $0`"
+
+curdir="`dirname $0`"
+mkdir "${curdir}/bin"
+cd bin
+bindirabs=`pwd -P` # absolute path to bindir
+cd ..
+
+for srcdir in openssl-0.9.8zh openssl-1.0.0s openssl-1.0.2q openssl-1.1.0j openssl-1.1.1a
+do
+  cd $srcdir
+  make clean
+  ./config shared --prefix="${bindirabs}/${srcdir}" --openssldir="${bindirabs}/${srcdir}"
+  make && make install
+  cd ..
+done
+
+
---
+++ test/run-on-many-lisps-and-openssls/openssl-releases/fetch-all.sh
@@ -0,0 +1,11 @@
+
+wget https://www.openssl.org/source/openssl-1.0.2q.tar.gz
+tar -xzf openssl-1.0.2q.tar.gz
+wget https://www.openssl.org/source/openssl-1.1.0j.tar.gz
+tar -xzf openssl-1.1.0j.tar.gz
+wget https://www.openssl.org/source/openssl-1.1.1a.tar.gz
+tar -xzf openssl-1.1.1a.tar.gz
+wget https://www.openssl.org/source/old/1.0.0/openssl-1.0.0s.tar.gz
+tar -xzf openssl-1.0.0s.tar.gz
+wget https://www.openssl.org/source/old/0.9.x/openssl-0.9.8zh.tar.gz
+tar -xzf openssl-0.9.8zh.tar.gz
