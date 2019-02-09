2d8b5ac7b01b843a4980332d2f1f8765500b648e
Anton Vodonosov <avodonosov@yandex.ru>
2019-02-09 07:53:35 +0300

    Ability to download and build only one OpenSSL release - useful for Travis CI

--- test/run-on-many-lisps-and-openssls/openssl-releases/build-all.sh
+++ test/run-on-many-lisps-and-openssls/openssl-releases/build-all.sh
@@ -4,9 +4,16 @@ curdir="$(dirname "$0")"
 mkdir "${curdir}/bin"
 cd "${curdir}/bin"
 bindirabs=`pwd -P` # absolute path to bindir
+
 cd ../src
 
-for srcdir in openssl-0.9.8zh openssl-1.0.0s openssl-1.0.2q openssl-1.1.0j openssl-1.1.1a
+VERSIONS="$1"
+if [ -z "$VERSIONS" ]
+then
+  VERSIONS="openssl-0.9.8zh openssl-1.0.0s openssl-1.0.2q openssl-1.1.0j openssl-1.1.1a"
+fi
+
+for srcdir in $VERSIONS
 do
   cd $srcdir
   make clean
--- test/run-on-many-lisps-and-openssls/openssl-releases/fetch-all.sh
+++ test/run-on-many-lisps-and-openssls/openssl-releases/fetch-all.sh
@@ -4,13 +4,26 @@ cd "$(dirname "$0")"
 mkdir src
 cd src
 
-wget https://www.openssl.org/source/openssl-1.0.2q.tar.gz
-tar -xzf openssl-1.0.2q.tar.gz
-wget https://www.openssl.org/source/openssl-1.1.0j.tar.gz
-tar -xzf openssl-1.1.0j.tar.gz
-wget https://www.openssl.org/source/openssl-1.1.1a.tar.gz
-tar -xzf openssl-1.1.1a.tar.gz
-wget https://www.openssl.org/source/old/1.0.0/openssl-1.0.0s.tar.gz
-tar -xzf openssl-1.0.0s.tar.gz
-wget https://www.openssl.org/source/old/0.9.x/openssl-0.9.8zh.tar.gz
-tar -xzf openssl-0.9.8zh.tar.gz
+VERSIONS="$1"
+if [ -z "$VERSIONS" ]
+then
+  VERSIONS="openssl-0.9.8zh openssl-1.0.0s openssl-1.0.2q openssl-1.1.0j openssl-1.1.1a"
+fi
+
+downloadUrl() {
+  version="$1"
+  case $version in
+      openssl-1.0.2q|openssl-1.1.0j|openssl-1.1.1a)
+          echo "https://www.openssl.org/source/${version}.tar.gz";;
+      openssl-1.0.0s)
+          echo "https://www.openssl.org/source/old/1.0.0/openssl-1.0.0s.tar.gz";;
+      openssl-0.9.8zh)
+          echo "https://www.openssl.org/source/old/0.9.x/openssl-0.9.8zh.tar.gz";;
+  esac
+}
+
+for version in $VERSIONS
+do
+  wget $(downloadUrl "$version")
+  tar -xzf "${version}.tar.gz"
+done
