1493ea06df45793f59bd598b4273e291916368f4
Anton Vodonosov <avodonosov@yandex.ru>
2019-07-05 06:23:19 +0300

    Preparing Travic CI configuration for 32bit Lisps and OpenSSL versions

--- test/run-on-many-lisps-and-openssls/openssl-releases/build.sh
+++ test/run-on-many-lisps-and-openssls/openssl-releases/build.sh
@@ -13,11 +13,17 @@ then
   VERSIONS="openssl-0.9.8zh openssl-1.0.0s openssl-1.0.2q openssl-1.1.0j openssl-1.1.1a"
 fi
 
+ARCH="$2"
+if [ -z "$ARCH" ]
+then
+  ARCH="x86_64"
+fi
+
 for srcdir in $VERSIONS
 do
   cd $srcdir
   make clean
-  ./config shared --prefix="${bindirabs}/${srcdir}" --openssldir="${bindirabs}/${srcdir}"
+  ./Configure shared --prefix="${bindirabs}/${srcdir}-${ARCH}" --openssldir="${bindirabs}/${srcdir}-${ARCH}" "linux-${ARCH}"
   make && make install
   cd ..
 done
