2c470143839ca3116db126232bcc5a941256387f
Mike Miller <mike@6502studios.com>
2020-09-02 15:01:57 -0400

    adds validity date test

---
+++ test/validity-dates.lisp
@@ -0,0 +1,16 @@
+;;;; -*- Mode: LISP; Syntax: COMMON-LISP; indent-tabs-mode: nil; coding: utf-8; show-trailing-whitespace: t -*-
+
+(in-package :cl+ssl.test)
+
+(def-suite :cl+ssl.validity-dates :in :cl+ssl
+  :description "Hostname verification tests")
+
+(in-suite :cl+ssl.validity-dates)
+
+(test validity-dates-google-cert
+  (with-cert ("google.der" cert)
+    (is (= (cl+ssl:get-not-after-time cert)
+           3641760000))
+    (is (= (cl+ssl:get-not-before-time cert)
+           3634055286))))
+
