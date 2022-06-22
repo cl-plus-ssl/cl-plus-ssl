b0a5c13f7d9a45bb839b4c4a4635f743c822f367
Anton Vodonosov <avodonosov@yandex.ru>
2022-06-23 02:48:00 +0300

    More OpenSSL versions in the build scripts

--- test/run-on-many-lisps-and-openssls/openssl-releases/fetch.sh
+++ test/run-on-many-lisps-and-openssls/openssl-releases/fetch.sh
@@ -7,7 +7,7 @@ cd src
 VERSIONS="$1"
 if [ -z "$VERSIONS" ]
 then
-  VERSIONS="openssl-0.9.8zh openssl-1.0.0s openssl-1.0.2q openssl-1.1.0j openssl-1.1.1a libressl-2.2.7 libressl-2.5.5 libressl-2.6.5 libressl-2.8.3 libressl-3.0.1"
+  VERSIONS="openssl-0.9.8zh openssl-1.0.0s openssl-1.0.2q openssl-1.1.0j openssl-1.1.1a openssl-1.1.1f openssl-1.1.1o libressl-2.2.7 libressl-2.5.5 libressl-2.6.5 libressl-2.8.3 libressl-3.0.1"
 fi
 
 downloadUrl() {
@@ -15,7 +15,7 @@ downloadUrl() {
   case $version in
       libressl*)
           echo "https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/${version}.tar.gz";;
-      openssl-1.0.2q|openssl-1.1.0j|openssl-1.1.1a)
+      openssl-1.0.2q|openssl-1.1.0j|openssl-1.1.1*)
           echo "https://www.openssl.org/source/${version}.tar.gz";;
       openssl-1.0.0s)
           echo "https://www.openssl.org/source/old/1.0.0/openssl-1.0.0s.tar.gz";;
