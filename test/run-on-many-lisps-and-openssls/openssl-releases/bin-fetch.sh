8451cbe26dae4277d16ad7bff3af34b7812860aa
Anton Vodonosov <avodonosov@yandex.ru>
2022-06-25 00:13:43 +0300

    CI (GitHub Actions) : the OpenSSL release binaries are in mounted directory instead of being part of docker image. re #121

--- test/run-on-many-lisps-and-openssls/openssl-releases/bin-fetch.sh
+++ test/run-on-many-lisps-and-openssls/openssl-releases/bin-fetch.sh
@@ -1,2 +1,3 @@
-curl https://cl-plus-ssl.common-lisp.dev/openssl-releases-bin.tgz
+curl https://cl-plus-ssl.common-lisp.dev/openssl-releases-bin.tgz --output openssl-releases-bin.tgz
 tar -xzf openssl-releases-bin.tgz
+rm openssl-releases-bin.tgz
