9360888b5aec0dd0d9cd050c2ad5878154987502
Anton Vodonosov <avodonosov@yandex.ru>
2022-06-25 04:03:18 +0300

    CI (GitHub Actions): avoid repeating complex parameters by introducing a one-liner script test.sh. re #121

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -2,4 +2,4 @@
 #
 # Just a one-liner script to avoid repeating so many parameters in many `run` sections of the test.yml
 
-docker run -e LISP -e LIB_LOAD_MODE -e OPENSSL -e BITS -u "$(id -u):$(id -g)" -i --mount type=bind,source=(realpath "$(dirname $0)/../../.."),target=/home/cl/ clfoundation/cl-devel:2022-02-09 /home/cl/cl-plus-ssl/test/run-for-ci.sh
+docker run -e LISP -e LIB_LOAD_MODE -e OPENSSL -e BITS -u "$(id -u):$(id -g)" -i --mount type=bind,source=$(realpath "$(dirname $0)/../../.."),target=/home/cl/ clfoundation/cl-devel:2022-02-09 /home/cl/cl-plus-ssl/test/run-for-ci.sh
