33b44196490bfd609c91c028a8acc71012b64677
Anton Vodonosov <avodonosov@yandex.ru>
2022-09-23 16:17:02 +0300

    example.lisp: improved comments, added ability to terminate the https server

--- examples/example.lisp
+++ examples/example.lisp
@@ -74,26 +74,44 @@
                  ;; Empty line means headers ended.
                  ;; (Don't try to read further expecting end of stream,
                  ;; because some servers, like google.com,
-                 ;; close the TCP socket without sending TLS close_notify alert
-                 ;; which causes OpenSSL to signal an error.
-                 ;; Such servers expect HTTP clients to use the protocol
-                 ;; information to determine how many bites to read,
-                 ;; instead of relying the connection termination.)
+                 ;; close the TCP socket without sending TLS close_notify alert,
+                 ;; and OpenSSL in this case signals an "Unexpected EOF"
+                 ;; error if we try to read.
+                 ;; Such servers expect HTTP clients to use the HTTP
+                 ;; protocol format to determine how many bytes to read,
+                 ;; instead of relying on the connection termination.)
                  ))
       (close https))))
 
-;; start a simple HTTPS server. See the mod_ssl documentation at
-;; <URL:http://www.modssl.org/> for information on generating the
-;; server certificate and key
+;; Start a simple HTTPS server.
 ;;
-;; You can stress-test the server with
+;; Simple self-signed key and certificate can be generated with
 ;;
-;;    siege -c 10 -u https://host:8080/foobar
+;;    openssl req -new -nodes -x509 -days 365 -subj / -keyout private-key.pem -outform PEM -out certificate.pem
 ;;
+;; For "real" certificates, you can use, for exammple, https://letsencrypt.org,
+;; or see the mod_ssl documentation at <URL:http://www.modssl.org/>
+;; (E.g. http://www.modssl.org/docs/2.8/ssl_faq.html)
+;;
+;; Query the server:
+;;
+;;   curl --insecure https://localhost:8080/foobar
+;;
+;; Stop the server:
+;;
+;;   curl --insecure https://localhost:8080/quit
+;;
+;; (the --insecure is for self-signed certificate)
+;;
+;; If you query this server with a self-signed certificate
+;; from browser, first time the browser will show a "Security Risk"
+;; error page and the server will break with "bad certificate alert"
+;; error. Then you can add a browser security exception
+;; from the "Security Risk" page, start the server again and re-open the URL.
 (defun test-https-server
     (&key (port 8080)
-       (cert "/home/david/newcert.pem")
-       (key "/home/david/newkey.pem"))
+       (cert "/home/anton/prj/cl+ssl/cl-plus-ssl/examples/certificate.pem")
+       (key "/home/anton/prj/cl+ssl/cl-plus-ssl/examples/private-key.pem"))
   (format t "~&SSL server listening on port ~d~%" port)
   (trivial-sockets:with-server (server (:port port))
     (loop
@@ -104,19 +122,24 @@
                       socket
                       :external-format '(:iso-8859-1 :eol-style :lf)
                       :certificate cert
-                      :key key)))
+                      :key key))
+             (quit nil))
         (unwind-protect
              (progn
                (loop :for line = (read-line-crlf client nil)
                      :while (> (length line) 1)
-                     :do (format t "HTTPS> ~a~%" line))
+                     :do (format t "HTTPS> ~a~%" line)
+                         (when (search "/quit" line)
+                           (setf quit t)))
                (format client "HTTP/1.0 200 OK~%")
-               (format client "Server: SSL-CMUCL/1.1~%")
+               (format client "Server: cl+ssl/examples/example.lisp/1.1~%")
                (format client "Content-Type: text/plain~%")
                (terpri client)
-               (format client "G'day at ~A!~%"
+               (format client "~:[G'day~;Bye~] at ~A!~%"
+                       quit
                        (multiple-value-list (get-decoded-time)))
                (format client "CL+SSL running in ~A ~A~%"
                        (lisp-implementation-type)
-                       (lisp-implementation-version)))
+                       (lisp-implementation-version))
+               (when quit (return)))
           (close client))))))
