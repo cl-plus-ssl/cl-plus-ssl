7f8cb861567ab41994a95cc7b5451f06ed9f9d57
Anton Vodonosov <avodonosov@yandex.ru>
2022-09-25 22:35:07 +0300

    examples/example.lisp - specify http server certificate and private key on context level, instead of reading them for every new request

--- examples/example.lisp
+++ examples/example.lisp
@@ -11,9 +11,9 @@
 (tls-example::test-https-client "www.google.com")
 
 ;; generate key and cert as explained in the test-https-server comments
-(tls-example::test-https-server :key "/path/to/private-key.pem"
-                                :cert "/path/to/certificate.pem"
-                                :passphrase "1234")
+(tls-example::test-https-server :cert-chain-file "/path/to/certificate.pem"
+                                :key-file "/path/to/private-key.pem"
+                                :key-password "1234")
 ;; test the server with curl or web browser as explained in the comments
 
 (tls-example::test-nntp-client)
@@ -95,44 +95,47 @@
 ;; from the "Security Risk" page, start the server again and re-open the URL.
 (defun test-https-server (&key
                             (port 8080)
-                            (cert "certificate.pem")
-                            (key "private-key.pem")
-                            (passphrase "1234"))
-  (format t "~&SSL server listening on port ~d~%" port)
-  (trivial-sockets:with-server (server (:port port))
-    (loop
-      (let* ((socket (trivial-sockets:accept-connection
-                      server
-                      :element-type '(unsigned-byte 8)))
-             (client (cl+ssl:make-ssl-server-stream
-                      socket
-                      :external-format '(:utf-8 :eol-style :crlf)
-                      :certificate cert
-                      :key key
-                      :password passphrase))
-             (quit nil))
-        (unwind-protect
-             (progn
-               ;; Read and log the request with its headers
-               (loop :for line = (read-line client nil)
-                     :while line
-                     :do (format t "HTTPS> ~a~%" line)
-                         (when (search "/quit" line)
-                           (setf quit t))
-                     :while (plusp (length line)))
-               ;; Write a response
-               (format client "HTTP/1.0 200 OK~%")
-               (format client "Server: cl+ssl/examples/example.lisp~%")
-               (format client "Content-Type: text/plain~%")
-               (terpri client)
-               (format client "~:[G'day~;Bye~] at ~A!~%"
-                       quit
-                       (multiple-value-list (get-decoded-time)))
-               (format client "CL+SSL running in ~A ~A~%"
-                       (lisp-implementation-type)
-                       (lisp-implementation-version))
-               (when quit (return)))
-          (close client))))))
+                            (cert-chain-file "certificate.pem")
+                            (key-file "private-key.pem")
+                            (key-password "1234"))
+  (let ((ctx (cl+ssl:make-context :certificate-chain-file cert-chain-file
+                                  :private-key-file key-file
+                                  :private-key-password key-password)))
+    (unwind-protect
+         (trivial-sockets:with-server (server (:port port))
+           (format t "~&SSL server listening on port ~d~%" port)
+           (loop
+             (let* ((socket (trivial-sockets:accept-connection
+                             server
+                             :element-type '(unsigned-byte 8)))
+                    (client (cl+ssl:with-global-context (ctx)
+                              (cl+ssl:make-ssl-server-stream
+                               socket
+                               :external-format '(:utf-8 :eol-style :crlf))))
+                    (quit nil))
+               (unwind-protect
+                    (progn
+                      ;; Read and log the request with its headers
+                      (loop :for line = (read-line client nil)
+                            :while line
+                            :do (format t "HTTPS> ~a~%" line)
+                                (when (search "/quit" line)
+                                  (setf quit t))
+                            :while (plusp (length line)))
+                      ;; Write a response
+                      (format client "HTTP/1.0 200 OK~%")
+                      (format client "Server: cl+ssl/examples/example.lisp~%")
+                      (format client "Content-Type: text/plain~%")
+                      (terpri client)
+                      (format client "~:[G'day~;Bye~] at ~A!~%"
+                              quit
+                              (multiple-value-list (get-decoded-time)))
+                      (format client "CL+SSL running in ~A ~A~%"
+                              (lisp-implementation-type)
+                              (lisp-implementation-version))
+                      (when quit (return)))
+                 (close client))))))
+    (cl+ssl:ssl-ctx-free ctx)))
 
 
 ;; Connect to an NNTP server, upgrade connection to TLS
