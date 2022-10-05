bc8cfc25b3c55a5ea56383a1480a55a0aae8d7f9
Anton Vodonosov <avodonosov@yandex.ru>
2022-10-06 00:14:50 +0300

    CI: reorder test matrix so that errors on different lisps are noticed sooner. re #121

--- .github/workflows/test-gen-matrix.lisp
+++ .github/workflows/test-gen-matrix.lisp
@@ -1,11 +1,12 @@
 ;; TODO: enable CLISP after # https://github.com/cl-plus-ssl/cl-plus-ssl/issues/163 is fixed
 (flet ((format-test-step (lisp openssl lib-load-mode)
-         (format t "      - if: success() || failure()~%")
-         (format t "        run: |~%")
+         (format t "      - run: |~%")
          (format t "           LISP=~A OPENSSL=~A BITS=64 LIB_LOAD_MODE=~A docker-home/cl-plus-ssl/.github/workflows/test.sh~%"
                  lisp openssl lib-load-mode)
          ;; Is 2 mins enough?
-         (format t "        timeout-minutes: 2~%"))
+         (format t "        timeout-minutes: 2~%")
+         (format t "        if: success() || failure()~%")
+         )
        (format-retrying-test-step (lisp openssl lib-load-mode)
          ;; Note the `<  /dev/null` at the end of the cmd-line.
          ;; This is needed to prevent CCL to hang waiting
@@ -18,8 +19,7 @@
          ;; Reported this as a bug: https://github.com/nick-fields/retry/issues/98
          (let ((cmd-line (format nil "LISP=~A OPENSSL=~A BITS=64 LIB_LOAD_MODE=~A docker-home/cl-plus-ssl/.github/workflows/test.sh < /dev/null"
                                  lisp openssl lib-load-mode)))
-           (format t "      - if: success() || failure()~%")
-           (format t "        uses: nick-fields/retry@v2.8.1~%")
+           (format t "      - uses: nick-fields/retry@v2.8.1~%")
            (format t "        name: Run with retries ~A~%" cmd-line)
            (format t "        with:~%")
            (format t "          command: |~%")
@@ -32,23 +32,30 @@
            ;; don't hide timeouts
            (format t "          retry_on: error~%")
            ;; don't hide error situations other than the known crashes
-           (format t "          retry_on_exit_code: 137~%"))))
-  (dolist (lisp '("sbcl" "ccl" "abcl"))
+           (format t "          retry_on_exit_code: 137~%")
+           (format t "        if: success() || failure()~%"))))
+  (dolist (lib-load-mode '("new" "old"))
     (dolist (openssl '(
-                       "openssl-0.9.8zh"
-                       "openssl-1.0.0s"
-                       "openssl-1.0.2q"
-                       "openssl-1.1.0j"
-                       "openssl-1.1.1p"
+                       ;; newest releases
                        "openssl-3.0.4"
-                       "libressl-2.2.7"
-                       "libressl-2.5.5"
-                       "libressl-2.6.5"
-                       "libressl-2.8.3"
-                       "libressl-3.0.1"
+                       "openssl-1.1.1p"
                        "libressl-3.5.3"
+
+                       ;; oldest releaes
+                       "openssl-0.9.8zh"
+                       "libressl-2.2.7"
+
+                       ;; the rest
+                       "openssl-1.1.0j"
+                       "openssl-1.0.2q"
+                       "openssl-1.0.0s"
+                       "libressl-3.5.3"
+                       "libressl-3.0.1"
+                       "libressl-2.8.3"
+                       "libressl-2.6.5"
+                       "libressl-2.5.5"
                        ))
-      (dolist (lib-load-mode '("new" "old"))
+      (dolist (lisp '("sbcl" "ccl" "abcl"))
         (unless (and (string= lisp "abcl")
                      (string= lib-load-mode "old"))
 
