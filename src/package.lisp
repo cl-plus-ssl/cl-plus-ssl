5f124bd97b41df846c23142ac88163921ffe9d54
Anton Vodonosov <avodonosov@yandex.ru>
2022-10-23 05:53:20 +0300

    Re-order the exported symbols in the package definition

--- src/package.lisp
+++ src/package.lisp
@@ -12,68 +12,98 @@
 (in-package :cl-user)
 
 (defpackage :cl+ssl
+  (:export
+
+   ;; Create TLS stream over TCP stream
+   #:make-ssl-client-stream
+   #:make-ssl-server-stream
+
+   ;; Custom CTX creation
+   #:make-context
+   #:ssl-ctx-free
+
+   ;; Custom binding for the global CTX
+   #:with-global-context
+
+   ;; Configure the global CTX
+   #:use-certificate-chain-file
+   #:ssl-load-global-verify-locations
+   #:ssl-set-global-default-verify-paths
+
+   ;; PEM file reading
+   #:with-pem-password
+
+   ;; Properties of an established TLS session
+   #:get-selected-alpn-protocol
+
+   ;; x509 Certificates
+   ;; Obtain
+   #:decode-certificate-from-file
+   #:decode-certificate
+   #:ssl-stream-x509-certificate
+   ;; Release
+   #:x509-free
+   ;; Accessors
+   #:certificate-not-after-time
+   #:certificate-not-before-time
+   #:certificate-subject-common-names
+   #:certificate-fingerprint
+   ;; (low level function, already
+   ;; employed by make-ssl-client-stream
+   ;; if verification is enabled and hostname
+   ;; is passed in)
+   #:verify-hostname
+
+   ;; Saving / loading Lisp image
+   #:reload
+
+   ;; Various
+   #:stream-fd
+   #:random-bytes
+   #:ensure-initialized
+
+   ;; Default values
+   #:*default-cipher-list*
+   #:*default-buffer-size*
+   #:*make-ssl-client-stream-verify-default*
+   #:*default-unwrap-stream-p*
+
+   ;; Error conditions.
+   ;; Not full list, there are more non-exported,
+   ;; including the base classes.
+   ;; Should we export them all?
+   #:ssl-error-verify
+   #:ssl-error-stream
+   #:ssl-error-code
+   #:ssl-error-initialize
+
+   ;; OpenSSL API constants
+   #:+ssl-verify-none+
+   #:+ssl-verify-peer+
+   #:+ssl-verify-fail-if-no-peer-cert+
+   #:+ssl-verify-client-once+
+
+   #:+ssl-op-no-sslv2+
+   #:+ssl-op-no-sslv3+
+   #:+ssl-op-no-tlsv1+
+   #:+ssl-op-no-tlsv1-1+
+   #:+ssl-op-no-tlsv1-2+
+
+   #:+ssl-sess-cache-off+
+   #:+ssl-sess-cache-client+
+   #:+ssl-sess-cache-server+
+   #:+ssl-sess-cache-both+
+   #:+ssl-sess-cache-no-auto-clear+
+   #:+ssl-sess-cache-no-internal-lookup+
+   #:+ssl-sess-cache-no-internal-store+
+   #:+ssl-sess-cache-no-internal+
+
+   ;; DEPRECATED.
+   ;; Use the (MAKE-SSL-CLIENT-STREAM .. :VERIFY ?) to enable/disable verification.
+   ;; MAKE-CONTEXT also allows to enab/disable verification.
+   #:ssl-check-verify-p
+   )
   (:use :common-lisp :trivial-gray-streams)
-  (:export #:*default-cipher-list*
-           #:ensure-initialized
-           #:reload
-           #:stream-fd
-           #:*default-buffer-size*
-           #:make-ssl-client-stream
-           #:*make-ssl-client-stream-verify-default*
-           #:get-selected-alpn-protocol
-           #:make-ssl-server-stream
-           #:*default-unwrap-stream-p*
-           #:use-certificate-chain-file
-           #:random-bytes
-           ;; DEPRECATED.
-           ;; Use the (MAKE-SSL-CLIENT-STREAM .. :VERIFY ?) to enable/disable verification.
-           ;; MAKE-CONTEXT also allows to enab/disable verification.
-           #:ssl-check-verify-p
-           #:ssl-load-global-verify-locations
-           #:ssl-set-global-default-verify-paths
-           #:ssl-error-verify
-           #:ssl-error-stream
-           #:ssl-error-code
-           #:ssl-error-initialize
-           #:ssl-ctx-free
-
-           #:with-pem-password
-
-           #:+ssl-verify-none+
-           #:+ssl-verify-peer+
-           #:+ssl-verify-fail-if-no-peer-cert+
-           #:+ssl-verify-client-once+
-
-           #:+ssl-op-no-sslv2+
-           #:+ssl-op-no-sslv3+
-           #:+ssl-op-no-tlsv1+
-           #:+ssl-op-no-tlsv1-1+
-           #:+ssl-op-no-tlsv1-2+
-
-           #:+ssl-sess-cache-off+
-           #:+ssl-sess-cache-client+
-           #:+ssl-sess-cache-server+
-           #:+ssl-sess-cache-both+
-           #:+ssl-sess-cache-no-auto-clear+
-           #:+ssl-sess-cache-no-internal-lookup+
-           #:+ssl-sess-cache-no-internal-store+
-           #:+ssl-sess-cache-no-internal+
-
-           #:make-context
-           #:with-global-context
-
-           ;; x509 stuff
-           #:decode-certificate-from-file
-           #:decode-certificate
-           #:ssl-stream-x509-certificate
-           #:certificate-not-after-time
-           #:certificate-not-before-time
-           #:certificate-subject-common-names
-           #:certificate-fingerprint
-           #:x509-free
-
-           ;; hostname-verification
-           #:verify-hostname)
   (:import-from :cl+ssl/config
                 #:libssl
                 #:libcrypto))
