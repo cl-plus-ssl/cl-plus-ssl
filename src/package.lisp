13d824e27cf7f6085086458daea1514b605b3980
Anton Vodonosov <avodonosov@yandex.ru>
2023-06-13 05:30:14 +0300

    package.lisp : more readable expor list (normal symbols instead of uninterned symbols), less hackish support of slime-edit-definition for the exported symbols + GitHub code navigation support for them

--- src/package.lisp
+++ src/package.lisp
@@ -7,109 +7,104 @@
 ;;;
 ;;; See LICENSE for details.
 
-(in-package :cl-user)
-
-;; For convenient browsing from SLIME using
-;; slime-edit-definition (M-.) for the exported symbols.
-;; SLIME looks for the most recent in-package
-;; to guess the package for the unqualified symbols.
-#+(or)
-(in-package #:cl+ssl)
-
 (defpackage :cl+ssl
-  (:export
+  (:use :common-lisp :trivial-gray-streams))
 
+(in-package :cl+ssl)
+
+(export
+ '(
    ;;; Create TLS stream over TCP stream
-   #:make-ssl-client-stream
-   #:make-ssl-server-stream
+   make-ssl-client-stream
+   make-ssl-server-stream
 
    ;;; Custom binding for the global SSL_CTX
-   #:with-global-context
+   with-global-context
 
    ;;; Custom SSL_CTX creation
-   #:make-context
-   #:ssl-ctx-free
+   make-context
+   ssl-ctx-free
 
    ;;; Configure the global SSL_CTX
-   #:use-certificate-chain-file
-   #:ssl-load-global-verify-locations
-   #:ssl-set-global-default-verify-paths
+   use-certificate-chain-file
+   ssl-load-global-verify-locations
+   ssl-set-global-default-verify-paths
 
    ;;; PEM file reading
-   #:with-pem-password
+   with-pem-password
 
    ;;; Properties of an established TLS session
-   #:get-selected-alpn-protocol
+   get-selected-alpn-protocol
 
    ;;; x509 Certificates
    ;;; Obtain
-   #:decode-certificate-from-file
-   #:decode-certificate
-   #:ssl-stream-x509-certificate
+   decode-certificate-from-file
+   decode-certificate
+   ssl-stream-x509-certificate
    ;;; Release
-   #:x509-free
+   x509-free
    ;;; Accessors
-   #:certificate-not-after-time
-   #:certificate-not-before-time
-   #:certificate-subject-common-names
-   #:certificate-fingerprint
+   certificate-not-after-time
+   certificate-not-before-time
+   certificate-subject-common-names
+   certificate-fingerprint
    ;; (low level function, already
    ;; employed by make-ssl-client-stream
    ;; if verification is enabled and hostname
    ;; is passed in)
-   #:verify-hostname
+   verify-hostname
 
    ;;; Saving / loading Lisp image
-   #:reload
+   reload
 
    ;;; Various
-   #:stream-fd
-   #:random-bytes
-   #:ensure-initialized
+   stream-fd
+   random-bytes
+   ensure-initialized
 
    ;;; Default values
-   #:*default-cipher-list*
-   #:*default-buffer-size*
-   #:*make-ssl-client-stream-verify-default*
-   #:*default-unwrap-stream-p*
+   *default-cipher-list*
+   *default-buffer-size*
+   *make-ssl-client-stream-verify-default*
+   *default-unwrap-stream-p*
 
    ;;; Error conditions.
    ;;; Not full list, there are more non-exported,
    ;;; including the base classes.
    ;;; Should we export them all?
-   #:ssl-error-verify
-   #:ssl-error-initialize
+   ssl-error-verify
+   ssl-error-initialize
    ;;; accessors of ssl-error-verify
-   #:ssl-error-stream
-   #:ssl-error-code
+   ssl-error-stream
+   ssl-error-code
 
    ;;; OpenSSL API constants
-   #:+ssl-verify-none+
-   #:+ssl-verify-peer+
-   #:+ssl-verify-fail-if-no-peer-cert+
-   #:+ssl-verify-client-once+
+   +ssl-verify-none+
+   +ssl-verify-peer+
+   +ssl-verify-fail-if-no-peer-cert+
+   +ssl-verify-client-once+
 
-   #:+ssl-op-no-sslv2+
-   #:+ssl-op-no-sslv3+
-   #:+ssl-op-no-tlsv1+
-   #:+ssl-op-no-tlsv1-1+
-   #:+ssl-op-no-tlsv1-2+
+   +ssl-op-no-sslv2+
+   +ssl-op-no-sslv3+
+   +ssl-op-no-tlsv1+
+   +ssl-op-no-tlsv1-1+
+   +ssl-op-no-tlsv1-2+
 
-   #:+ssl-sess-cache-off+
-   #:+ssl-sess-cache-client+
-   #:+ssl-sess-cache-server+
-   #:+ssl-sess-cache-both+
-   #:+ssl-sess-cache-no-auto-clear+
-   #:+ssl-sess-cache-no-internal-lookup+
-   #:+ssl-sess-cache-no-internal-store+
-   #:+ssl-sess-cache-no-internal+
+   +ssl-sess-cache-off+
+   +ssl-sess-cache-client+
+   +ssl-sess-cache-server+
+   +ssl-sess-cache-both+
+   +ssl-sess-cache-no-auto-clear+
+   +ssl-sess-cache-no-internal-lookup+
+   +ssl-sess-cache-no-internal-store+
+   +ssl-sess-cache-no-internal+
 
-   ;; DEPRECATED.
+   ;;; DEPRECATED.
+
    ;; Use the (MAKE-SSL-CLIENT-STREAM .. :VERIFY ?) to enable/disable verification.
    ;; MAKE-CONTEXT also allows to enab/disable verification.
-   #:ssl-check-verify-p
-   )
-  (:use :common-lisp :trivial-gray-streams)
-  (:import-from :cl+ssl/config
-                #:libssl
-                #:libcrypto))
+   ssl-check-verify-p
+   ))
+
+(import '(cl+ssl/config::libssl
+          cl+ssl/config::libcrypto))
